ca216ff4a11026d643f5f5302ea386e2bc03244d
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-09-23 18:23:08 +0100

    wip: test offline

--- machines/stunning-fantail/run.sh
+++
@@ -1,2 +0,0 @@
-cd /home/adrian-hesketh/stunning-fantail
-nix run github:numtide/system-manager/e8957ab8b4cf02574adb5f09ef4a2ef9ee48ef01 -- switch --flake .
--- machines/worker-001/flake.nix
+++ machines/worker-001/flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/23.05";
 
     system-manager = {
-      url = "github:numtide/system-manager";
+      url = "github:numtide/system-manager/e8957ab8b4cf02574adb5f09ef4a2ef9ee48ef01";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
